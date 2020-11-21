098947b275879554e53daee5a2ae3ee4185fc10d
Matt Raymond <matt@Matts-MacBook-Pro.local>
2020-11-21 14:54:31 -0500

    fixed merge issues

--- test_server/app.js
+++ test_server/app.js
@@ -8,7 +8,7 @@ const test = require('./public/javascripts/testing_functions');
 const FileSync = require('lowdb/adapters/FileSync')
 var router = require('./routes/index');
 var users = require('./routes/users');
-const low = require('lowdb')
+const low = require('lowdb');
 
 // database
 const adapter = new FileSync('db.json')
@@ -29,8 +29,9 @@ const socket1 = dgram.createSocket('udp4');
 const socket2 = dgram.createSocket('udp4');
 
 socket1.on('message', (message, rinfo) => {
+  console.log('received state');
   let data = test.extract_data(message);
-
+  
   let datagram_contents = {
     ip: rinfo.address,
     source_port: rinfo.port,
@@ -38,29 +39,30 @@ socket1.on('message', (message, rinfo) => {
     time_sent: data.date,
     contents: data.buffer.toString()
   };
-
+  
   let client_id = `${rinfo.address}_${rinfo.port}`;
-
+  
   let user_exists = db.get('client')
-    .filter({ client: client_id }).value() == [];
-
+  .filter({ client: client_id }).value() == [];
+  
   if (!user_exists) {
     db.get('client')
-      .push({ client: client_id, messages: [] })
-      .write();
-  };
-
-  db.get("client")
-    .find({ client: client_id })
-    .get("messages")
-    .push(datagram_contents)
+    .push({ client: client_id, messages: [] })
     .write();
-
+  };
+  
+  db.get("client")
+  .find({ client: client_id })
+  .get("messages")
+  .push(datagram_contents)
+  .write();
+  
   io.emit('update', {
     client: client_id,
     message: datagram_contents
   });
-
+  
+  console.log('saved state to local database');
 });
 
 socket2.on('message', (message, rinfo) => {
@@ -71,9 +73,11 @@ socket1.bind(rec_orig_state);
 socket2.bind(rec_new_state);
 
 const testing_socket = dgram.createSocket('udp4');
-testing_socket.send(test.generateBuffer(), 0, 256, rec_orig_state, '0.0.0.0', () => {
-  console.log('sent');
-});
+setTimeout(() => {
+  testing_socket.send(test.generateBuffer(), 0, 256, rec_orig_state, '0.0.0.0', () => {
+    console.log('sent simulated message');
+  });
+}, 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -117,6 +121,7 @@ io.on('connection', (socket) => {
 
   
   socket.on('send_state_init', data => {
+    console.log('sending state to new client');
 
     // ToDo: not checking for the app id, but probably should
    let packet = db.get('client')
