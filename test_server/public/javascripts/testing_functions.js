d6490769d93ac8baa417f895a5c92a3c70a6c3fc
Matt Raymond <m4tt.r4ymond@gmail.com>
2020-12-15 23:14:08 -0500

    updated code to not have lengths

--- test_server/public/javascripts/testing_functions.js
+++ test_server/public/javascripts/testing_functions.js
@@ -38,17 +38,17 @@ const readFile = (filename, header) => {
     return addIdentifier(contents, header)
 }
 
-const addIdentifier = (contents, header, add_len = true) => {
-    var offset = 1 + add_len*2;
+const addIdentifier = (contents, header) => {
+    // var offset = 1;// + add_len*2;
 
-    combined = Buffer.alloc(contents.length + offset);
+    combined = Buffer.alloc(contents.length + 1);
     combined.writeUInt8(header, 0);
 
-    if(add_len)
-        combined.writeUInt16BE(contents.length, 1);
+    // if(add_len)
+    //     combined.writeUInt16BE(contents.length, 1);
 
     for (let i = 0; i < contents.length; i++) {
-        combined.writeUInt8(contents[i], i + offset);
+        combined.writeUInt8(contents[i], i + 1);
     }
 
     return combined;
